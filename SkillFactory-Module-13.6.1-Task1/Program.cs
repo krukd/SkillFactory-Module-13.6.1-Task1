66ffe92e2501fa0d854f6990ee58d0c7b3305e6d
krukd <krukd87@gmail.com>
2023-07-03 21:01:27 +0200

    Добавьте файлы проекта.

---
+++ SkillFactory-Module-13.6.1-Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace SkillFactory_Module_13._6._1_Task1
+{
+    internal class Program
+    {
+        public static List<string> usualList = new List<string>();
+        public static LinkedList<string> linkedList = new LinkedList<string>();
+
+        static void Main(string[] args)
+        {
+
+            string text = File.ReadAllText("\\\\Mac\\Home\\Desktop\\Text1.txt");
+
+            char[] delimiters = new char[] { ' ', '\r', '\n', '.', ',', ';', ':', '!', '?', '"' };
+
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            Stopwatch stopWatch1 = new Stopwatch();
+
+            stopWatch1.Start();                    
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                linkedList.AddLast(words[i]);
+            }
+
+            stopWatch1.Stop();
+
+            Console.WriteLine($"Время добавления текста в связанный список: {stopWatch1.Elapsed.TotalMilliseconds} мс");
+
+
+            Stopwatch stopWatch2 = new Stopwatch();
+            stopWatch2.Start();                    
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                usualList.Add(words[i]);
+            }
+
+            stopWatch2.Stop();
+
+            Console.WriteLine($"Время добавления текста в список: {stopWatch2.Elapsed.TotalMilliseconds} мс");
+
+
+        }
+    }
+}
